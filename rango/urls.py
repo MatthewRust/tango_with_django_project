5f7627b1a87161a1b7845aa386c523dba5ec986c
Matthew Rust <2888890r@student.gla.ac.uk>
2025-01-30 23:13:34 +0000

    Chapter 3 work

---
+++ rango/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rango import views
+from django.urls import include
+from django.contrib import admin 
+
+
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+
+]
