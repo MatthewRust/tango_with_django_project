a212c010fa30aa386d38670bf79a53a05e4514c8
Matthew Rust <2888890r@student.gla.ac.uk>
2025-02-14 14:40:11 +0000

    Chapter 9 with working tests

--- rango/models.py
+++ rango/models.py
@@ -38,4 +38,4 @@ class UserProfile(models.Model):
     picture = models.ImageField(upload_to='profile_images', blank=True)
 
     def __str__(self):
-        return super().user.username
+        return self.user.username
